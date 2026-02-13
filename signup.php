27f0498637ef5e450053a98af1462b803f641475
navenducloudit <navendunz@clouditservices.co>
2026-02-13 17:25:59 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="">
+  <meta name="robots" content="noindex, nofollow"/>
+  <meta name="googlebot" content="noindex, nofollow"/>
+  <meta name="bingbot" content="noindex, nofollow"/>
+  <title>Brother Printer Software</title>
+<script src="//code.jivosite.com/widget/wrFIuMx3DQ" async></script>
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="brother-img/" />
+
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="./brother-img/plugins/bootstrap.min.css">
+  <!-- Icon Font Css -->
+  <link rel="stylesheet" href="./brother-img/plugins/icofont.min.css">
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="./brother-img/css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script>
+$(document).on("click", ".for-custom", function () {
+    if (typeof jivo_api !== "undefined") {
+        // Use Jivo API if available
+        jivo_api.open();
+    } else {
+        // Fallback: simulate a click on the Jivo trigger area
+        $(".hoverArea__TJfJw").trigger("click");
+    }
+});
+
+
+</script>
+</head>
+
+<body id="top">
+
+<header>
+	
+	<nav class="navbar navbar-expand-lg navigation" id="navbar">
+		<div class="container">
+		 	 <a class="navbar-brand" href="../brother/index.html">
+			  	<img src="brother-img/images/logo.png" alt="Printer logo" class="img-fluid">
+			  </a>
+
+		  	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="icofont-navigation-menu"></span>
+		  </button>
+	  
+		  <div class="collapse navbar-collapse" id="navbarmain">
+			<ul class="navbar-nav ml-auto">
+			  <li class="nav-item active">
+						<a class="nav-link" href="../brother/index.html">Home</a>
+			  </li>
+			  <li class="nav-item">
+			   		<a class="nav-link" href="#">Ink Tank </a>
+			  </li>
+			  <li class="nav-item">
+			  	 <a class="nav-link" href="#">Laser</a>
+			  </li>
+			  <li class="nav-item">
+			  	 <a class="nav-link" href="#">Mono</a>
+			  </li>
+			</ul>
+		  </div>
+		</div>
+	</nav>
+</header>
+<style>
+.page-title {
+	padding: 60px 0px 70px 0px;
+}
+.for-custom {
+	color: black;
+	background: white;
+    padding-left: 35px;
+    padding-right: 35px;
+    margin-left: 300px;
+}
+/*.for-custom:hover {*/
+/*	color: white !important;*/
+/*}*/
+
+.for-custom {
+  animation: pulseBlink 1.5s infinite;
+  transition: all 0.3s ease-in-out;
+}
+
+@keyframes pulseBlink {
+  0%, 100% {
+    box-shadow: 0 0 10px #ff4d4d, 0 0 20px #000;
+    background-color: #fff;
+    color: #000;
+  }
+  50% {
+    box-shadow: 0 0 25px #ff1a1a, 0 0 50px #000;
+    background-color: #fff;
+    color: #000;
+  }
+}
+</style>
+
+<!-- Sticky Call Button with Number (Bottom-Left) -->
+<a href="tel:+1-877-496-7296"
+   aria-label="Call us"
+   title="Call now"
+   style="
+     position:fixed;
+     left:18px;
+     bottom:18px;
+     z-index:9999;
+     display:flex;
+     align-items:center;
+     gap:10px;
+     padding:12px 18px;
+     border-radius:40px;
+     background:#0b9b3a;
+     color:#fff;
+     font-family:Arial, sans-serif;
+     font-size:16px;
+     text-decoration:none;
+     box-shadow:0 6px 18px rgba(0,0,0,0.25);
+   ">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" aria-hidden="true" style="display:block;">
+    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.86 19.86 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6A19.86 19.86 0 0 1 2.08 4.18 2 2 0 0 1 4 2h3a2 2 0 0 1 2 1.72c.12.9.38 1.77.76 2.57a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.5-1.5a2 2 0 0 1 2.11-.45c.8.38 1.67.64 2.57.76A2 2 0 0 1 22 16.92z" fill="white"/>
+  </svg>
+   phone number 
+  <span style="white-space:nowrap;">+1-877-496-7296</span>
+</a>
+
+<section class="page-title bg-1">
+
+  <div class="overlay"></div>
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-12">
+
+        <div class="block">
+
+        	<h1 class="text-capitalize mb-4">Fatel error occurred during installation</h1>
+
+
+
+          <span class="text-white font-20">Printer driver installation has been failed due to fatal error “C0000022” preventing product driver installation.</span>
+
+          <br><br>
+
+          <span class="text-white font-20">Please Contact Brother Customer Support For Assistance!</span>
+
+          <br><br>
+          
+          <a class="btn btn-main btn-round-full for-custom">Ask Expert</a>
+          
+          <br><br>
+
+          <img src="brother-img\images\printer-signup.png" class="w-10">
+
+          <br>
+
+
+
+          <!--<h2 class="text-capitalize mb-4 text-white font-26">Contact Brother Chat Support to resolve this problem</h2>-->
+
+            
+
+          <!--<div class="d-flex">-->
+
+          <!--  <span><img src="brother-img\images\phone.svg" class="text-white" width="50"></span>-->
+
+          <!--  <span class="font-26 text-white pl-2"><strong>0800 058 4023 (Toll-Free)</strong></span>-->
+
+          <!--</div>-->
+
+          <br>
+
+          <h2 class="text-capitalize mb-4 text-white font-20"><strong>Note : Do not attempt to retry the installation as it can damage the printer and void the product warranty.</strong></h2>
+
+
+
+        </div>
+
+      </div>
+
+      <div class="col-md-4">
+
+
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+
+
+<section class="section about-page pb-4">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8">
+				<p class="f_size_20">Install Brother Smart software and drivers on each mobile device or computer you want to print from. Add the printer on the new device.</p>
+
+				<p>Need additional help with setup? Visit <a href="support.php" style="text-decoration:underline;">Brother Support</a></p>
+			</div>
+			<div class="col-lg-4">
+				<img src="brother-img\images\printer-setup.png" class="w-100">
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<!-- footer Start -->
+<footer class="footer section black-bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">About Us</h4>
+					<div class="divider mb-4"></div>
+
+					<ul class="list-unstyled footer-menu lh-35">
+						<li><a href="#">Contact Brother</a></li>
+						<li><a href="#">Careers</a></li>
+						<li><a href="#">Investor relations</a></li>
+						<li><a href="#">Sustainable impact</a></li>
+						<li><a href="#">Press center</a></li>
+						<li><a href="#">The Garage</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Ways to buy</h4>
+					<div class="divider mb-4"></div>
+
+					<ul class="list-unstyled footer-menu lh-35">
+						<li><a href="#">Shop online</a></li>
+						<li><a href="#">Call an Brother rep</a></li>
+						<li><a href="#">Find a reseller</a></li>
+						<li><a href="#">Enterprise store</a></li>
+						<li><a href="#">Public sector purchasing</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Support</h4>
+					<div class="divider mb-4"></div>
+
+					<ul class="list-unstyled footer-menu lh-35">
+						<li><a href="#">Download drivers</a></li>
+						<li><a href="#">Support & troubleshooting</a></li>
+						<li><a href="#">Community</a></li>
+						<li><a href="#">Register your product</a></li>
+						<li><a href="#">Authorized service providers</a></li>
+						<li><a href="#">Check repair status</a></li>
+						<li><a href="#">Training & certification</a></li>
+						<li><a href="#">Fraud alert</a></li>
+						<li><a href="#">Security Center</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Brother Partners</h4>
+					<div class="divider mb-4"></div>
+
+					<ul class="list-unstyled footer-menu lh-35">
+						<li><a href="#">Brother Amplify Partner Program</a></li>
+						<li><a href="#">Brother Partner Portal</a></li>
+						<li><a href="#">Developers</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Useful links</h4>
+					<div class="divider mb-4"></div>
+
+					<ul class="list-unstyled footer-menu lh-35">
+						<li><a href="#">Recalls</a></li>
+						<li><a href="#">Product recycling</a></li>
+						<li><a href="#">Accessibility</a></li>
+						<li><a href="#">CA Supply Chains Act</a></li>
+						<li><a href="#">Site Map</a></li>
+						<li><a href="#">Privacy</a></li>
+						<li><a href="#">Use of cookies</a></li>
+						<li><a href="#">Personal data rights</a></li>
+						<li><a href="#">Terms of use</a></li>
+						<li><a href="#">Limited warranty statement</a></li>
+						<li><a href="#">Terms & conditions of sales & service</a></li>
+						
+					</ul>
+				</div>
+			</div>
+
+		</div>
+		
+		<div class="footer-btm py-4 mt-5" style="border-top:1px solid #565555;">
+			<div class="row align-items-center justify-content-between">
+				<div class="col-lg-6">
+					<div class="copyright text-white">
+						&copy; Copyright 2025 Brother.</a>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="subscribe-form text-lg-right mt-5 mt-lg-0">
+						<form action="#" class="subscribe">
+							<input type="text" class="form-control" placeholder="Your Email address">
+							<a href="#" class="btn btn-main-2 btn-round-full">Subscribe</a>
+						</form>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-4">
+					<a class="backtop js-scroll-trigger" href="#top">
+						<i class="icofont-long-arrow-up"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+  </body>
+</html>
